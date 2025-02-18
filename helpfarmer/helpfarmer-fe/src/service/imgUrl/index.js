9557023887f3076ddbf1bc36de3de84fecc1624b
960707270@qq.com <960707270@qq.com>
2025-02-18 22:47:54 +0800

    modified:   helpfarmer/helpfarmer-fe/src/service/imgUrl/index.js

---
+++ helpfarmer/helpfarmer-fe/src/service/imgUrl/index.js
@@ -0,0 +1,54 @@
+import instance from '../instance'
+//获取import instance from '../instance';
+
+// 获取商品列表
+export const list = (pageNo, pageSize, addForm) => {
+  return instance.get('/goods/list', { params: { pageNo, pageSize, addForm } })
+}
+
+// 添加商品
+export const add = addForm => {
+  const formData = new FormData()
+  for (const key in addForm) {
+    if (key === 'images' && Array.isArray(addForm[key])) {
+      addForm[key].forEach(file => formData.append('images', file))
+    } else {
+      formData.append(key, addForm[key])
+    }
+  }
+  return instance.post('/goods/add', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+}
+
+// 更新商品
+export const update = addForm => {
+  const formData = new FormData()
+  for (const key in addForm) {
+    if (key === 'images' && Array.isArray(addForm[key])) {
+      addForm[key].forEach(file => formData.append('images', file))
+    } else {
+      formData.append(key, addForm[key])
+    }
+  }
+  return instance.put('/goods/update', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+}
+
+// 删除商品
+export const deletes = id => {
+  return instance.get(`/goods/delete/${id}`)
+}
+// 上传图片
+export const uploadImage = formData => {
+  return instance.post('/goods/uploadImage', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+}
